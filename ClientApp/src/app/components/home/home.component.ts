21f4f4383f6ae705f8d83e853bf8edddcd9b77d6
toolbasedotdev <toolbasedotdev@proton.me>
2022-05-31 18:31:44 -0700

    Add route data listener

--- ClientApp/src/app/components/home/home.component.ts
+++ ClientApp/src/app/components/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { AccountService } from "src/app/services/account.service";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+    constructor(
+        private route: ActivatedRoute,
+        private account: AccountService
+    ) {}
 
-    constructor() { }
+    private _routeDataSub: Subscription | null = null;
 
     ngOnInit(): void {
+        this._routeDataSub = this.route.data.subscribe(
+            (data) => {
+                if (data?.command) {
+                    switch (data.command) {
+                        case "signout":
+                            this.account.signOut();
+                            break;
+                    }
+                }
+            },
+            (err) => {
+                throw err;
+            }
+        );
     }
 
+    ngOnDestroy(): void {
+        if (this._routeDataSub) this._routeDataSub.unsubscribe();
+    }
 }
