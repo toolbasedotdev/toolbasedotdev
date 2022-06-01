f2cfd6099285a7ef6330779bcf5f156bd53f1c17
toolbasedotdev <toolbasedotdev@proton.me>
2022-05-31 18:18:30 -0700

    Update proxy to fix routing

--- ClientApp/proxy.conf.js
+++ ClientApp/proxy.conf.js
@@ -1,21 +1,31 @@
 /* eslint-disable no-undef */
-const { env } = require('process');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { env } = require("process");
 
 // eslint-disable-next-line no-nested-ternary
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
-  : env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:13995';
+const target = env.ASPNETCORE_HTTPS_PORT
+    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+    : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(";")[0]
+    : "http://localhost:13995";
 
 const PROXY_CONFIG = [
-  {
-    context: [
-      '/',
-    ],
-    target,
-    secure: false,
-    headers: {
-      Connection: 'Keep-Alive',
+    {
+        context: ["/api/**"],
+        target,
+        secure: false,
+        headers: {
+            Connection: "Keep-Alive",
+        },
     },
-  },
+    {
+        context: ["/signin-github"],
+        target,
+        secure: false,
+        headers: {
+            Connection: "Keep-Alive",
+        },
+    },
 ];
 
 module.exports = PROXY_CONFIG;
