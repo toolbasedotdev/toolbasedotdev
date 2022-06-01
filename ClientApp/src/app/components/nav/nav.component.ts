20a27f9d181cd2c637e37b3c9e2ace224ca26ddd
toolbasedotdev <toolbasedotdev@proton.me>
2022-05-31 19:40:30 -0700

    Add logo to nav

--- ClientApp/src/app/components/nav/nav.component.ts
+++ ClientApp/src/app/components/nav/nav.component.ts
@@ -1,5 +1,14 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
+import {
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from "@angular/core";
+import { NavigationEnd, Router, RouterEvent } from "@angular/router";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
 import { AccountService } from "src/app/services/account.service";
 import { GithubService } from "src/app/services/github.service";
 import { NavMenuComponent } from "../nav-menu/nav-menu.component";
@@ -9,15 +18,40 @@ import { NavMenuComponent } from "../nav-menu/nav-menu.component";
     templateUrl: "./nav.component.html",
     styleUrls: ["./nav.component.scss"],
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy {
     faGithub = faGithub;
-    constructor(public account: AccountService, public github: GithubService) {}
+    constructor(
+        private router: Router,
+        public account: AccountService,
+        public github: GithubService
+    ) {}
+
+    private _routeUrlSub: Subscription | null = null;
 
     @ViewChild("accountMenu")
     public accountMenu!: NavMenuComponent;
 
     @ViewChild("accountMenuButton")
     public accountMenuButton: ElementRef | undefined;
+
+    public url = "/";
+
+    ngOnInit(): void {
+        this._routeUrlSub = this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe(
+                (event) => {
+                    this.url = (event as NavigationEnd).urlAfterRedirects;
+                },
+                (err) => {
+                    throw err;
+                }
+            );
+    }
+
+    ngOnDestroy(): void {
+        if (this._routeUrlSub) this._routeUrlSub.unsubscribe();
+    }
 
     signInGithub(): void {
         this.account.signIn();
