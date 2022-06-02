ac251b7241597c34c65ff627afa68b7f71bd25a3
toolbasedotdev <toolbasedotdev@proton.me>
2022-06-01 18:39:33 -0700

    Style

--- Models/GithubAccessDetails.cs
+++ Models/GithubAccessDetails.cs
@@ -1,7 +1,17 @@
 namespace ToolBaseDotDev.Models;
 
+/// <summary>
+/// Contains details required for client-side Github GraphQL API access.
+/// </summary>
 public struct GithubAccessDetails
 {
+    /// <summary>
+    /// ClientId of the Github OAuth app.
+    /// </summary>
     public string ClientId { get; set; }
+
+    /// <summary>
+    /// Access token granted by Github OAuth.
+    /// </summary>
     public string AccessToken { get; set; }
 }
--- Models/UserDetails.cs
+++ Models/UserDetails.cs
@@ -1,13 +1,23 @@
 namespace ToolBaseDotDev.Models;
 
+/// <summary>
+/// Details relevant to displaying a user.
+/// Does not include user data.
+/// </summary>
 public class UserDetails
 {
+    /// <summary>
+    /// Specifies that the user was not found.
+    /// </summary>
     public bool NotFound { get; set; }
+
+    /// <summary>
+    /// The username, which is also used as the display name.
+    /// </summary>
     public string? Name { get; set; }
 
-    public static UserDetails UserNotFound() => new UserDetails
-    {
-        NotFound = true,
-        Name = null
-    };
+    /// <summary>
+    /// Default value to return when an attempt to find a user fails.
+    /// </summary>
+    public static UserDetails UserNotFound() => new() { NotFound = true };
 }
