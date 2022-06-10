3aa4a931b9db7df95f0f6ef1e76d1b744a42e34f
toolbasedotdev <toolbasedotdev@proton.me>
2022-06-09 18:25:52 -0700

    Create SectionListComponent

---
+++ ClientApp/src/app/modules/settings/components/section-list/section-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+/**
+ * A list used to navigate all the settings sub-pages.
+ */
+@Component({
+    selector: "settings-section-list",
+    templateUrl: "./section-list.component.html",
+    styleUrls: ["./section-list.component.scss"],
+})
+export class SectionListComponent {}
