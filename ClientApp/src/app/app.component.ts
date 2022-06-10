45e28881d29c0e674bfa8a7876ebde8d426d333a
toolbasedotdev <toolbasedotdev@proton.me>
2022-06-09 18:25:38 -0700

    Add auto redirect from auth'd routes

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivationStart, Data, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 import { AccountService } from "./services/account/account.service";
 import { GithubService } from "./services/github/github.service";
 import { ReadyService } from "./services/ready/ready.service";
@@ -11,7 +13,7 @@ import { ReadyService } from "./services/ready/ready.service";
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     /**
      * App title.
      */
@@ -21,15 +23,27 @@ export class AppComponent implements OnInit {
      * Constructs the AppComponent.
      */
     constructor(
+        private _router: Router,
         private _ready: ReadyService,
         private _account: AccountService,
         private _github: GithubService
     ) {}
 
     /**
-     * On init, syncs the local user and Github API access details.
+     * Subscription: Router events.
+     */
+    private _routerSub: Subscription | null = null;
+
+    /**
+     * On init: syncs the local user and Github API access details.
      */
     public ngOnInit(): void {
+        this._routerSub = this._router.events.subscribe((event) => {
+            if (event instanceof ActivationStart) {
+                this.onRouterData(event.snapshot.data);
+            }
+        });
+
         this._ready.await(async () => {
             await this._account.syncLocalUser();
 
@@ -37,5 +51,25 @@ export class AppComponent implements OnInit {
                 await this._github.syncAccessToken();
             }
         });
+    }
+
+    /**
+     * On destroy: Unsubscribes from router events.
+     */
+    public ngOnDestroy(): void {
+        if (this._routerSub) {
+            this._routerSub.unsubscribe();
+        }
+    }
+
+    /**
+     * Fires when router data changes.
+     */
+    public async onRouterData(data: Data): Promise<void> {
+        await this._ready.toPromise();
+
+        if (data.auth && !this._account.isSignedIn()) {
+            this._router.navigate(["/"]);
+        }
     }
 }
